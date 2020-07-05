4900555cb3b31723151f3972f565a22c1bed8032
Naafi <naafi96@gmail.com>
2020-07-05 13:16:44 +0530

    Code Optimized and UI Updated

---
+++ script.js
@@ -0,0 +1,15 @@
+// AUTHOR		:	ABDUL WAHID NAAFI
+// INSTAGRAM	:	@IAM_NAAFI 
+// INSTAGRAM 	:	@NAAFICODES
+var x=document.getElementById("hands");
+var y=document.getElementById("animcon");
+function closeye()
+{
+	y.style.backgroundImage="url('images/monkey_pwd.gif')";
+	x.style.marginTop="0%";
+}
+function openeye()
+{
+	y.style.backgroundImage="url('images/monkey.gif')";
+	x.style.marginTop="110%";
+}
